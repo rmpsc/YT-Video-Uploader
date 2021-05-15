d4977982f14dea17447daf103d70eefba9787ffe
Romel Pascua <romelpascua14@gmail.com>
2021-05-15 16:06:13 -0700

    request_body and response_upload

--- uploader.py
+++ uploader.py
@@ -1,5 +1,5 @@
 import datetime
-from google import Create_Service
+from create import Create_Service
 from googleapiclient.http import MediaFileUpload
 
 CLIENT_SECRET_FILE = 'client_secret.json'
@@ -9,11 +9,32 @@ SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
+upload_date_time = datetime.datetime(2021, 5, 16, 12, 00, 0).isoformat() + '.000Z'
+
 request_body  = {
     'snippet': {
-        'categoryI': 20,
-        'title': 'Upload Testing',
+        'categoryId': 20,
+        'title': 'Word but Valorant',
         'description': 'word.exe description',
         'tags': ['Valorant', 'Sheesh']
     },
+    'status': {
+        'privacyStatus': 'private',
+        'publishAt': upload_date_time,
+        'selfDeclaredMadeForKids': False,
+    },
+    'notifySubscribers': False
 }
+
+mediaFile = MediaFileUpload('wordexe.mp4')
+
+response_upload = service.videos().insert(
+    part='snippet,status',
+    body=request_body,
+    media_body=mediaFile
+).execute()
+
+service.thumbnails().set(
+    videoId=response_upload.get('id'),
+    media_body=MediaFileUpload('jett_icon.png')
+).execute()
