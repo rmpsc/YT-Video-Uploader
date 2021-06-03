4be32690ae39428c139a3bbfdae9682847fd3727
Romel Pascua <romelpascua14@gmail.com>
2021-06-03 12:20:58 -0700

    change datetime/add mp4

--- uploader.py
+++ uploader.py
@@ -9,12 +9,12 @@ SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-upload_date_time = datetime.datetime(2021, 5, 16, 12, 00, 0).isoformat() + '.000Z'
+upload_date_time = datetime.datetime(2021, 6, 3, 12, 00, 0).isoformat() + '.000Z'
 
 request_body  = {
     'snippet': {
         'categoryId': 20,
-        'title': 'Word but Valorant',
+        'title': 'Audit public test',
         'description': 'word.exe description',
         'tags': ['Valorant', 'Sheesh']
     },
